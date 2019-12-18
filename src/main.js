2e7b5f87b9a8406e7fc208e5f4a88de6a0ebc639
ayang <747312515@qq.com>
2019-12-18 17:13:06 +0800

    3页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-import './style/index.less'
+import './styles/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
+    redirect: '/login'
+  }, {
+    path: '/login',
+    component: Login
+  }, {
+    path: '/home',
     component: Home
   }
   // {
