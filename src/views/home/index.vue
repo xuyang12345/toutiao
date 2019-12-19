8124bb169ba3db6cd2bd1a463338486c8052e862
ayang <747312515@qq.com>
2019-12-19 21:54:19 +0800

    主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="login">
-     <span>我是主页</span>
-  </div>
+    <el-container>
+    <el-aside style="background-color:#353b4e;height:100vh;width:230px">
+      <!-- 放置左侧组件 -->
+      <layout-aside></layout-aside>
+    </el-aside>
+       <el-container>
+            <el-header>
+              头部
+              <layout-header></layout-header>
+            </el-header>
+            <el-main>
+              主要区域
+            </el-main>
+       </el-container>
+    </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside.vue' // 引入
+import layoutHeader from '../../components/home/layout-header' // 引入
 export default {
-
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
 }
 </script>
 
