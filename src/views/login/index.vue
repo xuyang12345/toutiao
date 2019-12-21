af67c27f4ed2e22c66c2ac30510881a542d9b94e
ayang <747312515@qq.com>
2019-12-21 20:45:03 +0800

    面包屑组件封装

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -69,12 +69,6 @@ export default {
           }).then(result => {
             window.localStorage.setItem('user-token', result.data.token)
             this.$router.push('/home')
-          }).catch(() => {
-            this.$message({
-              message: '您的手机号或者验证码不正确',
-              type: 'warning'
-
-            })
           })
         }
       })
