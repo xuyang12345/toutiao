19a355f47be088bb2589de7d775a077157cec565
ayang <747312515@qq.com>
2019-12-22 17:05:43 +0800

    评论数据加载到页面及状态过滤

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -38,7 +38,7 @@ export default {
       //   headers参数
 
     }).then(result => {
-      console.log(result)
+      // console.log(result)
       this.userInfo = result.data // 获取用户个人信息
     })
   },
